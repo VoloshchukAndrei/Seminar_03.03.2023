66abab7d4af508ec612103f9e9a63878b79e7c76
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-03-04 10:24:24 +0500

    Task051 - Сумма элементов главной диагонали

---
+++ Task051/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 51: Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся
+//  на главной диагонали (с индексами (0,0); (1; 1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns);
+
+PrintArray(array);
+
+Console.WriteLine($"Сумма элементов главной диагонали: {diagonalSum(array)}");
+
+int[,] GetArray(int M, int N)
+{
+    int[,] result = new int[M, N];
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            result[i, j] = new Random().Next(1, 20 + 1);
+        }
+    }
+    return result;
+}
+
+int diagonalSum(int[,] N)
+{
+    int sum = 0;
+    for (int i = 0; i < N.GetLength(0); i++)
+    {
+        for (int j = 0; j < N.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + N[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[,] N)
+{
+    for (int i = 0; i < N.GetLength(0); i++)
+    {
+        for (int j = 0; j < N.GetLength(1); j++)
+        {
+            Console.Write($"{N[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
